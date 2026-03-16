851f6037e8ee00f05e4967ac26716571e53ae735
Oedada <Oedada>
2026-03-16 23:08:19 +0300

    test broadcast

--- src/broadcasts.cpp
+++ src/broadcasts.cpp
@@ -11,8 +11,6 @@
 const char MagicMessage[] = "Wifisync Hello Wifi";
 const char MagicResponse[] = "Wifisync Response Wifi";
 
-int PORT = 55555;
-
 class UdpBroadcast{
     public:
         int broadcast_port;
@@ -22,15 +20,10 @@ class UdpBroadcast{
         sockaddr_in own_addr{};
         UdpBroadcast(int p) : broadcast_port(p){
             broadcast_sock = socket(AF_INET, SOCK_DGRAM, 0);
-            // set broadcast sendto address
             broadcast_addr.sin_family = AF_INET;
             broadcast_addr.sin_port = htons(broadcast_port);
-            inet_pton(AF_INET, "255.255.255.255", &broadcast_addr.sin_addr);
-            //set local receive address
-            sockaddr_in local_addr{};
-            local_addr.sin_family = AF_INET;
-            local_addr.sin_port = htons(PORT);
-            inet_pton(AF_INET, "0.0.0.0", &local_addr.sin_addr);
+            std::string broadcast_ip = "255.255.255.255";
+            inet_pton(AF_INET, broadcast_ip.c_str(), &broadcast_addr.sin_addr);
             if(bind(broadcast_sock, (sockaddr*)&broadcast_addr, sizeof(broadcast_addr)) != 0){
                 throw std::runtime_error("Can't bind broadcast sock to addr");
             }
@@ -79,7 +72,6 @@ class UdpBroadcast{
                 socklen_t sender_len = sizeof(tmp_addr);
                 ssize_t n = recvfrom(broadcast_sock, buf, sizeof(buf)-1, 0,(sockaddr*)&tmp_addr, &sender_len);
                 if (n > 0) {
-                    // buf[n] = '\0';
                     if(tmp_addr.sin_addr.s_addr != own_addr.sin_addr.s_addr){
                         if(std::strcmp(MagicMessage, buf) == 0){
                                 other_addr = tmp_addr;
@@ -107,7 +99,6 @@ int main() {
     UdpBroadcast b(12345);
     while(!b.recieve()){
         b.send_msg(MagicMessage, sizeof(MagicMessage));
-        // usleep(200000);
     }
     char ip[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &b.other_addr.sin_addr, ip, sizeof(ip));
