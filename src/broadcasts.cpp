831b2af01296168710b57c9731974ec27bc858eb
Oedada <Oedada>
2026-03-16 22:18:09 +0300

    test broadcast

--- src/broadcasts.cpp
+++ src/broadcasts.cpp
@@ -71,8 +71,8 @@ class UdpBroadcast{
                 socklen_t sender_len = sizeof(other_addr);
                 ssize_t n = recvfrom(broadcast_sock, buf, sizeof(buf)-1, 0,(sockaddr*)&tmp_addr, &sender_len);
                 if (n > 0) {
-                    std::cout << "Received" << "\n";
                     if(tmp_addr.sin_addr.s_addr != own_addr.sin_addr.s_addr){
+                        std::cout << "Received" << "\n";
                         buf[n] = '\0';
                         std::cout << buf << "\n";
                         if(std::strcmp(MagicMessage, buf) == 0){
