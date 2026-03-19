07d484aba65ee4d30732f478e43218b338c9107a
Oedada <Oedada>
2026-03-19 22:21:14 +0300

    debaging broadcast finish

--- src/broadcasts.cpp
+++ src/broadcasts.cpp
@@ -1,4 +1,3 @@
-#include <cstddef>
 #include <cstdint>
 #include <net/if.h>
 #include <ifaddrs.h>
@@ -9,161 +8,156 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include "headers/broadcast.hpp"
 
+UdpBroadcast::UdpBroadcast(int p) : broadcast_port(p){
+    if(broadcast_sock < 0){
+        throw std::runtime_error("Can't creat socket");
+    }
+    // get own addr and broadcast addr
+    if(!get_own_and_brcast_addr(own_addr, broadcast_addr)){
+        get_own_ip();
+        inet_pton(AF_INET, "255.255.255.255", &broadcast_addr.sin_addr); 
+    }
+    // broadcast addr
+    broadcast_addr.sin_family = AF_INET;
+    broadcast_addr.sin_port = htons(broadcast_port);
+    //local addr
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_port = htons(broadcast_port);
+    local_addr.sin_addr.s_addr = INADDR_ANY;
+    //bind
+    if(bind(broadcast_sock, (sockaddr*)&local_addr, sizeof(local_addr)) != 0){
+        throw std::runtime_error("Can't bind broadcast sock to addr");
+    }
+    //enable broadcast
+    int enable = 1;
+    setsockopt(broadcast_sock, SOL_SOCKET, SO_BROADCAST, &enable, sizeof(enable));
+}
+void UdpBroadcast::send_broadcast(){
+    sendto(broadcast_sock, MagicMessage, strlen(MagicMessage), 0,(sockaddr*)&broadcast_addr, sizeof(broadcast_addr));
+}
 
-const char MagicMessage[] = "Wifisync Hello Wifi";
-const char MagicResponse[] = "Wifisync Response Wifi";
-const int PORT = 12345;
-const int SLEEP_TIME = 200000;
+bool UdpBroadcast::is_suitable_interface_name(char *name){
+    std::string n = name;
+    return !(n.find("lo") != std::string::npos ||
+            n.find("tun") != std::string::npos ||
+            n.find("docker") != std::string::npos ||
+            n.find("veth") != std::string::npos ||
+            n.find("zt") != std::string::npos ||
+            n.find("br-") != std::string::npos);
+}
 
-class UdpBroadcast{
-    public:
-        int broadcast_port;
-        sockaddr_in broadcast_addr{};
-        sockaddr_in other_addr{};
-        sockaddr_in own_addr{};
-        sockaddr_in local_addr{};
-        int broadcast_sock = socket(AF_INET, SOCK_DGRAM, 0);
-        UdpBroadcast(int p) : broadcast_port(p){
-            // get own addr and broadcast addr
-            get_own_and_brcast_addr(own_addr, broadcast_addr);
-            // broadcast addr
-            broadcast_addr.sin_family = AF_INET;
-            broadcast_addr.sin_port = htons(broadcast_port);
-            //local addr
-            local_addr.sin_family = AF_INET;
-            local_addr.sin_port = htons(broadcast_port);
-            local_addr.sin_addr.s_addr = INADDR_ANY;
-            //bind
-            if(bind(broadcast_sock, (sockaddr*)&local_addr, sizeof(local_addr)) != 0){
-                throw std::runtime_error("Can't bind broadcast sock to addr");
-            }
-            //enable broadcast
-            int enable = 1;
-            setsockopt(broadcast_sock, SOL_SOCKET, SO_BROADCAST, &enable, sizeof(enable));
+void UdpBroadcast::print_ip(sockaddr_in addr){
+    char ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &addr.sin_addr, ip, sizeof(ip));
+    std::cout << " IP: " << ip << std::endl;
+}
+
+void UdpBroadcast::calculate_broadcast_addr(struct ifaddrs* ifa, sockaddr_in &baddr){
+    auto* addr = (struct sockaddr_in*)ifa->ifa_addr;
+    auto* mask = (struct sockaddr_in*)ifa->ifa_netmask;
+    uint32_t ip_addr = addr->sin_addr.s_addr;
+    uint32_t net_mask = mask->sin_addr.s_addr;
+    uint32_t broadcast = ip_addr | ~net_mask;
+    baddr.sin_addr.s_addr = broadcast;
+}
+
+bool UdpBroadcast::get_own_and_brcast_addr(sockaddr_in &own_addr, sockaddr_in &baddr){
+    struct ifaddrs* ifaddr;
+
+    if(getifaddrs(&ifaddr) == -1){
+        throw std::runtime_error("Can't get ifaddr");
+    }
+    for(struct ifaddrs* ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next){
+        if (!ifa->ifa_addr) continue;
+        if(!is_suitable_interface_name(ifa->ifa_name)){
+            continue;
         }
-        void send_msg(const char* msg, size_t s){
-            sendto(broadcast_sock, msg, s, 0,(sockaddr*)&broadcast_addr, sizeof(broadcast_addr));
+        if (!ifa->ifa_netmask) continue;
+        if (ifa->ifa_addr->sa_family != AF_INET) continue;
+        if(!(ifa->ifa_flags & IFF_UP)) continue;
+        calculate_broadcast_addr(ifa, baddr);
+        own_addr = *(sockaddr_in*)ifa->ifa_addr;
+        freeifaddrs(ifaddr);
+        return true;
+    }
+    freeifaddrs(ifaddr);
+    return false;
+}
+
+void UdpBroadcast::get_own_ip(){
+    int tmp_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    sockaddr_in tmp_addr{};
+    tmp_addr.sin_family = AF_INET;
+    tmp_addr.sin_port = htons(53);
+    inet_pton(AF_INET, "8.8.8.8", &tmp_addr.sin_addr);
+
+    if(connect(tmp_sock, (sockaddr*)&tmp_addr, sizeof(tmp_addr)) != 0){
+        inet_pton(AF_INET, "192.0.2.1", &tmp_addr.sin_addr);
+        if(connect(tmp_sock, (sockaddr*)&tmp_addr, sizeof(tmp_addr)) != 0){
+            throw std::runtime_error("Can't build route for know own ip");
         }
+    }
+    socklen_t own_addr_size = sizeof(own_addr); 
+    getsockname(tmp_sock, (sockaddr*)&own_addr, &own_addr_size);
+    close(tmp_sock);
+}
 
-        bool is_suitable_interface_name(char *name){
-            std::string n = name;
-            return !(n.find("lo") != std::string::npos ||
-                    n.find("tun") != std::string::npos ||
-                    n.find("docker") != std::string::npos ||
-                    n.find("veth") != std::string::npos ||
-                    n.find("zt") != std::string::npos ||
-                    n.find("br-") != std::string::npos);
-        }
+bool UdpBroadcast::is_ready_to_recv(){
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(broadcast_sock, &readfds);
 
-        void print_ip(sockaddr_in addr){
-            char ip[INET_ADDRSTRLEN];
-            inet_ntop(AF_INET, &addr.sin_addr, ip, sizeof(ip));
-            std::cout << " IP: " << ip << std::endl;
-        }
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 200000;
+    
+    int ret = select(broadcast_sock + 1, &readfds, nullptr, nullptr, &timeout);
 
-        void calculate_broadcast_addr(struct ifaddrs* ifa, sockaddr_in &baddr){
-            auto* addr = (struct sockaddr_in*)ifa->ifa_addr;
-            auto* mask = (struct sockaddr_in*)ifa->ifa_netmask;
-            uint32_t ip_addr = addr->sin_addr.s_addr;
-            uint32_t net_mask = mask->sin_addr.s_addr;
-            uint32_t broadcast = ip_addr | ~net_mask;
-            baddr.sin_addr.s_addr = broadcast;
-        }
+    if (ret < 0) {
+        perror("select");
+        return false;
+    }
+    if(ret > 0 && FD_ISSET(broadcast_sock, &readfds)){
+        return true;
+    }
+    return false;
+}
 
-        bool get_own_and_brcast_addr(sockaddr_in &own_addr, sockaddr_in &baddr){
-            struct ifaddrs* ifaddr;
-
-            if(getifaddrs(&ifaddr) == -1){
-                throw std::runtime_error("Can't get ifaddr");
-            }
-            for(struct ifaddrs* ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next){
-                if (!ifa->ifa_addr) continue;
-                if(!is_suitable_interface_name(ifa->ifa_name)){
-                    continue;
+bool UdpBroadcast::recieve(){
+    if(is_ready_to_recv()){
+        char buf[1024];
+        sockaddr_in tmp_addr;
+        socklen_t sender_len = sizeof(tmp_addr);
+        ssize_t n = recvfrom(broadcast_sock, buf, sizeof(buf)-1, 0,(sockaddr*)&tmp_addr, &sender_len);
+        if (n > 0) {
+            if(tmp_addr.sin_addr.s_addr != own_addr.sin_addr.s_addr){
+                if(n == strlen(MagicMessage) && memcmp(buf, MagicMessage, strlen(MagicMessage)) == 0){
+                        other_addr = tmp_addr;
+                        sendto(broadcast_sock, MagicResponse, strlen(MagicResponse), 0,(sockaddr*)&other_addr, sizeof(other_addr));
+                        return true;
                 }
-                if (ifa->ifa_addr->sa_family != AF_INET) continue;
-                if(!(ifa->ifa_flags & IFF_UP)) continue;
-                calculate_broadcast_addr(ifa, baddr);
-                own_addr = *(sockaddr_in*)ifa->ifa_addr;
-                freeifaddrs(ifaddr);
-                return true;
-            }
-            freeifaddrs(ifaddr);
-            return false;
-        }
-
-        void get_own_ip(){
-            int tmp_sock = socket(AF_INET, SOCK_DGRAM, 0);
-            sockaddr_in tmp_addr{};
-            tmp_addr.sin_family = AF_INET;
-            tmp_addr.sin_port = htons(53);
-            inet_pton(AF_INET, "8.8.8.8", &tmp_addr.sin_addr);
-
-            if(connect(tmp_sock, (sockaddr*)&tmp_addr, sizeof(tmp_addr)) != 0){
-                inet_pton(AF_INET, "192.0.2.1", &tmp_addr.sin_addr);
-                if(connect(tmp_sock, (sockaddr*)&tmp_addr, sizeof(tmp_addr)) != 0){
-                    throw std::runtime_error("Can't build route for know own ip");
-                }
-            }
-            socklen_t own_addr_size = sizeof(own_addr); 
-            getsockname(tmp_sock, (sockaddr*)&own_addr, &own_addr_size);
-            close(tmp_sock);
-        }
-
-        bool is_ready_to_recv(){
-            fd_set readfds;
-            FD_ZERO(&readfds);
-            FD_SET(broadcast_sock, &readfds);
-
-            struct timeval timeout;
-            timeout.tv_sec = 0;
-            timeout.tv_usec = 200000;
-            
-            int ret = select(broadcast_sock + 1, &readfds, nullptr, nullptr, &timeout);
-
-            if(ret > 0 && FD_ISSET(broadcast_sock, &readfds)){
-                return true;
-            }
-            return false;
-        }
-
-        bool recieve(){
-            if(is_ready_to_recv()){
-                char buf[1024];
-                sockaddr_in tmp_addr;
-                socklen_t sender_len = sizeof(tmp_addr);
-                ssize_t n = recvfrom(broadcast_sock, buf, sizeof(buf)-1, 0,(sockaddr*)&tmp_addr, &sender_len);
-                if (n > 0) {
-                    if(tmp_addr.sin_addr.s_addr != own_addr.sin_addr.s_addr){
-                        if(n == strlen(MagicMessage) && memcmp(buf, MagicMessage, strlen(MagicMessage)) == 0){
-                                other_addr = tmp_addr;
-                                sendto(broadcast_sock, MagicResponse, strlen(MagicResponse), 0,(sockaddr*)&other_addr, sizeof(other_addr));
-                                return true;
-                        }
-                        else if(n == strlen(MagicResponse) && memcmp(buf, MagicResponse, strlen(MagicResponse)) == 0){
-                                other_addr = tmp_addr;
-                                return true;
-                        }
-                    }
+                else if(n == strlen(MagicResponse) && memcmp(buf, MagicResponse, strlen(MagicResponse)) == 0){
+                        other_addr = tmp_addr;
+                        return true;
                 }
             }
-            return false;
         }
+    }
+    return false;
+}
 
-        ~UdpBroadcast(){ 
-            close(broadcast_sock);
-        }
-};
+UdpBroadcast::~UdpBroadcast(){ 
+    close(broadcast_sock);
+}
 
 
 int main() {
     UdpBroadcast b(PORT);
     char ip[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &b.own_addr.sin_addr, ip, sizeof(ip));
-    std::cout << ip << "\n";
     while(!b.recieve()){
-        b.send_msg(MagicMessage, strlen(MagicMessage));
+        b.send_broadcast();
         usleep(SLEEP_TIME);
     }
     inet_ntop(AF_INET, &b.other_addr.sin_addr, ip, sizeof(ip));
---
+++ src/headers/broadcast.hpp
@@ -0,0 +1,36 @@
+#include <net/if.h>
+#include <ifaddrs.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+
+const char MagicMessage[] = "Wifisync Hello Wifi";
+const char MagicResponse[] = "Wifisync Response Wifi";
+const int PORT = 12345;
+const int SLEEP_TIME = 200000;
+
+class UdpBroadcast{
+    public:
+        sockaddr_in other_addr{};
+        UdpBroadcast(int p);
+        void send_broadcast();
+
+        void print_ip(sockaddr_in addr);
+
+        bool recieve();
+
+        ~UdpBroadcast();
+    private:
+        void get_own_ip();
+        bool get_own_and_brcast_addr(sockaddr_in &own_addr, sockaddr_in &baddr);
+        bool is_ready_to_recv();
+        void calculate_broadcast_addr(struct ifaddrs* ifa, sockaddr_in &baddr);
+        bool is_suitable_interface_name(char *name);
+        int broadcast_sock = socket(AF_INET, SOCK_DGRAM, 0);
+        sockaddr_in own_addr{};
+        sockaddr_in local_addr{};
+        int broadcast_port;
+        sockaddr_in broadcast_addr{};
+};
